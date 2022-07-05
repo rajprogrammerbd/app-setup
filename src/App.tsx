2eab2f6395115d7a86c12973c64030d8bfddb951
Raj Dutta <rd2249619@gmail.com>
2022-07-05 11:11:57 +0600

    modify to test the OTP

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,69 @@
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable react/state-in-constructor */
 import React from 'react';
 
-function App() {
-  const [state, setState] = React.useState({ name: 'default' });
-  React.useEffect(() => {
-    if ('OTPCredential' in window) {
-      window.addEventListener('DOMContentLoaded', () => {
-        navigator.credentials.get({
-          otp: { transport: ['sms'] },
-        }).then((otp: any) => {
-          setState({ name: otp.code });
-        }).catch(() => {
-          setState({ name: 'something is fishy' });
-        });
-      });
-    }
-  }, []);
+export default class App extends React.Component {
+  state = {
+    otp: '',
+  };
 
-  return (
-    <form>
-      <input style={{ border: '1px solid #ddd' }} type="text" autoComplete="one-time-code" onChange={(e: any) => setState(e.target.value)} value={state.name} required />
-      <input type="submit" />
-    </form>
-  );
+  componentDidMount() {
+    if ('OTPCredential' in window) {
+      const ac = new AbortController();
+
+      navigator.credentials
+        .get({
+          otp: { transport: ['sms'] },
+          signal: ac.signal,
+        })
+        .then((otp: any) => {
+          this.setState({ otp: otp.code });
+          ac.abort();
+        })
+        .catch((err) => {
+          ac.abort();
+          console.log(err);
+        });
+    }
+  }
+
+  render() {
+    const { otp } = this.state;
+    return (
+      <div className="App">
+        <br />
+        <br />
+        <br />
+        <br />
+        <h2>
+          Your OTP is:
+          {' '}
+          {otp}
+        </h2>
+        <br />
+        <br />
+        <br />
+        <br />
+        <h3>The Web OTP API Docs</h3>
+        <div>
+          Send an SMS that includes
+          <pre>
+            <code>@web-otp-shravan.netlify.app #12345</code>
+          </pre>
+          at the last line to this phone.
+          <pre>
+            <code>please don't use https:// and / end of the url</code>
+          </pre>
+          <pre style={{ color: 'green' }}>
+            <code>example for use @www.google.com</code>
+          </pre>
+          <pre style={{ color: 'red' }}>
+            <code>example for not use XXXXX @https://www.google.com XXXXX</code>
+          </pre>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+// time to test in mobile phone
